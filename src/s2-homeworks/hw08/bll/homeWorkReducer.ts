df45678a991783c448bd3794e9f162589f4fb815
vladnen97 <darkdescent1991@gmail.com>
2023-03-15 19:41:54 +0300

    hw8: refactoring

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -8,9 +8,9 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
     switch (action.type) {
         case 'sort': { // by name
             if (action.payload === 'up') {
-                return [...state.sort((a, b) => a.name > b.name ? 1 : -1)]
+                return [...state].sort((a, b) => a.name > b.name ? 1 : -1)
             }
-            return [...state.sort((a, b) => a.name < b.name ? 1 : -1)]
+            return [...state].sort((a, b) => a.name < b.name ? 1 : -1)
         }
         case 'check': {
             return state.filter(el => el.age > +action.payload);
