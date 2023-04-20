bfeb5ee99ee936dadc7ebf20280d246a9ce5a26d
vladnen97 <darkdescent1991@gmail.com>
2023-04-20 17:47:15 +0300

    HW12: fix any type, write case

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,17 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: ReturnType<typeof changeThemeId>):  {themeId: number} => { // fix any
     switch (action.type) {
         // дописать
-
+        case 'SET_THEME_ID':
+            return {
+                ...state,
+                themeId: action.id
+            }
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): {type: 'SET_THEME_ID', id: number} => ({ type: 'SET_THEME_ID', id }) // fix any
